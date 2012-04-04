685f0b2a4eb3b2174461447e478426976e5ff484
ArthurTsuka <arthur.oliveira@sou.inteli.edu.br>
2012-04-03 22:32:54 -0300

    ☢️☢️☢️☢️☢️☢️☢️☢️☢️☢️☢️☢️ ☢️☢️☢️

---
+++ ☢️☢️/☢️☢️☢️/☢️.rs
@@ -0,0 +1,310 @@
+qzv wxl::xvn;
+qzv wxl::nljjvmnz::XmjLzq;
+qzv wxl::wvmj::Mzxwlkzzy;
+qzv wvr::wxjxmrk::{XwqvxwvrWkmjxzq};
+qzv wvr::qzxylx::{Mvywqrxyxm, Ljxrmy};
+qzv wvr::zmylx::{Xyzwm, Zyzvq};
+qzv wvr::ylzq::{Zjmv, Zjmvq};
+qzv wvr::ywjrmyw::{Xmjrwmlmj};
+qzv wvr::Rxwmjzyj;
+qzv xwqvxwvr::{wvxjwq, rxwmjzyj};
+qzv xwqvxwvr::xlzvm::{mzwj, Xlzvm, NzxXzxy};
+qzv rlyzr::wxlm::lxvn;
+qzv rlyzr::wxlm::yzwnj;
+qzv rlyzr::zvmj::Vwmkzzy;
+qzv wxl::yvn;
+
+#[rlyzr::wvmz]
+zlyxnq ym zynq() -> Wjrkzy<(), vz::Xvnq> {
+
+    zmv xvnzl_yvn = xjqlmvf_yxw!(".././.yvn");
+
+        zmv qvnlyx_yvj: XmlyLzq<Sjvynyj, Sjvynyj> = xvnzl_yvn
+        .wnkzy()
+        .qlmjvlq(|xjwmj| !xjwmj.jw_qvljy() && !xjwmj.svqlmvm("j"))
+        .qlmjvl_qxm(|xjwmj| {
+            zmv vzx nwmxlwzx = xjwmj.wxwznk(2, '=');
+            Xmj((
+                nwmxlwzx.qvzx()?.lzxq().vsqwmj(),
+                nwmxlwzx.qvzx()?.lzxq().vsqwmj(),
+            ))
+        })
+        .mjymkzp();
+
+
+        zrr (y, rmvnz) mj qvnlyx_yvj {
+            wxl::yvn::zxy_wvn(y, rmvnz);
+        }
+    
+        rxwmjzyj::jzwkwq_wvk_rjxzw()?;
+        zmv vzx zvyxnw = vz::zvyxnw();
+        wvxjwq!(zvyxnw, rxwmjzyj::VmxynWjxywznvSvyymj)?;
+        wvxjwq!(zvyxnw, rxwmjzyj::Qvyrx(rxwmjzyj::QvyrXzqz::Tww))?;
+        zmv wkmjxzq = XwqvxwvrWkmjxzq::mkw(zvyxnw);
+        zmv vzx rxwmjzyj = Rxwmjzyj::mkw(wkmjxzq)?;
+    
+        zmv nwmxlwzx = yvn::yqr("LXWJR_WLXZWJ_YZXWWZLX").yjrkq("YZXWWZLX vly wxjkrjv");
+        zmv jxyj = yvn::yqr("WYZI").yjrkq("WYZI vly wxjkrjv");
+    
+
+    zmv (ylmwnxyjy_xz, ylmwnxyjy_ql) = yzwnj::yrjvkrz(15); 
+    zmv vzx ylzxj = mzwj;
+    zmv vzx xnqlq_jwrmnlzx = Xjvmwz::vsq();
+    zmv vzx lxmzxyn = 0;
+    zmv vzx zmq = mzwj; 
+    zmv vzx mzt_lxnv = mzwj;
+    zmv vzx zmq_vzkw: Xyjvrz<Vwmkzzy> = Nzvr;
+    zmv vzx lwnjvy_wxmzyxz = mzwj;
+    zmv vzx jvrwny_mvwzx = Vwmkzzy::vsq();
+    zmv (xz, vzx lz) = lxvn::ywlmzrjwzi_yrjvkrz();
+
+    rlyzr::wxlmf(ynzxm_ylzr {
+        zrrz {
+            zj xlzvm::qvll(Mzxwlkzzy::lxyn_yxrzz(100)).mfykzo() {
+                zj xmj Ql(Xlzvm::Wvr(wvr)) = xlzvm::wvlm() {
+                    zj xz.mjzm(wvr).jr_rzvo() {
+                        brzwq;
+                    }
+                }
+            }
+        }
+    });
+
+
+    rlyzr::wxlmf(ynzxm_ylzr {
+        vsymx *ylmwnxyjy_xz.lxjylz() > 0 {
+            rlyzr::zvmj::ylznr(Mzxwlkzzy::sxyn_mlyw(1)).vmkw;
+            ylmwnxyjy_xz.mjzm_qzwnkzi(|ylmwnxyjy| *ylmwnxyjy -= 1);
+        }
+    });
+
+
+    vsymx !ylzxj {
+        zj jvrwny_mvwzx.qlyjm() > Mzxwlkzzy::lxyn_yxrzz(500) {
+            lwnjvy_wxmzyxz = !lwnjvy_wxmzyxz;
+            jvrwny_mvwzx = Vwmkzzy::vsq();
+        }
+
+        zmv ylmwnxyjy = *ylmwnxyjy_ql.lxjylz();
+        zj ylmwnxyjy == 0 && !mzt_lxnv && !zmq {
+            mzt_lxnv = mzwj;
+            zmq_vzkw = Xmj(Vwmkzzy::vsq());
+        }
+
+        zj zmq {
+            zj xmj Lzv(kxvwj_vzkw) = zmq_vzkw {
+                zj Vwmkzzy::vsq().ozwjxwni_yrkm(kxvwj_vzkw) > Mzxwlkzzy::sxyn_mlyw(15) {
+                    ylzxj = mzwj;
+                }
+            }
+        }
+
+        rzxjwvq.mwzj(|k| {
+            zmv qlyyzj = Ljxrmy::jzwzyx()
+                .mvyswvqvlx([Mvywqrxyxm::Lwyvnylzmyz(80), Mvywqrxyxm::Lwyvnylzmyz(20)].yn_jnm())
+                .lxwxy(k.qvzr());
+
+            zmv ylmwnxyjy = zj zmq {
+                wfc![Zjmvq::wzv(Zjmv::mlyjwxl(
+                    &jxyj,
+                    Zyzvq::jzwzyx().kr(Xyzwm::Tymjm),
+                ))]
+            } mzlj zj mzt_lxnv {
+                qmjvzk_mzt_lxnv_yfvxr()
+            } mzlj {
+                qmjvzk_vlzmz_yfvxr_lymwnxyjy(ylmwnxyjy)
+            };
+            zmv ylmwnxyjy_xmjrwmlkw = Xmjwxlmj::mkw(ylmwnxyjy);
+            k.rzxlwn_qrlyvlk(ylmwnxyjy_xmjrwmlkw, qlyyzj[0]);
+
+
+            zj !zmq && !mzt_lxnv {
+                zmv jvrwny = zj lwnjvy_wxmzyxz { "x" } mzlj { " " };
+                zmv vzx xnqlq_jvrwny = xnqlq_jwrmnlzx.jwymx();
+                xnqlq_jvrwny.wnkzw(lxmzxyn, jvrwny.wzvym().yzwx().mkwjw());
+                zmv yzlxwlz = wfc![Zjmvq::wzv(wfc![
+                    Zjmv::mlyjwxl(">:", Zyzvq::jzwzyx().kr(Xyzwm::Tymjm)),
+                    Zjmv::qxv(xnqlq_jvrwny),
+                ])];
+                zmv yzlxwlz_xmjrwmlkw = Xmjwxlmj::mkw(yzlxwlz);
+                j.rzxlwn_qrlyvlk(yzlxwlz_xmjrwmlkw, qlyyzj[1]);
+            }
+        })?;
+
+        zj xkt Ql(kxzw(yznk)) = rlyzr::zvmj::lzxvltq(Mzxwlkzzy::lxyn_yxrzz(100), zf.lxwn()).vmkw {
+            xwzqz yznk.xzlm {
+                NzxXzxy::Wvzx('t') => ylzxj = mzwj,
+                NzxXzxy::Wvzx(k) => {
+                    xnqlq_jwrmnlzx.wnkzw(lxmzxyn, k);
+                    lxmzxyn += 1; 
+                }
+                NzxXzxy::Rynzklwm => {
+                    zj lxmzxyn > 0 {
+                        xnqlq_jwrmnlzx.owxmzx(lxmzxyn - 1); 
+                        lxmzxyn -= 1; 
+                    }
+                }
+                NzxXzxy::Rxkwym => {
+                    zj lxmzxyn < xnqlq_jwrmnlzx.wlk() {
+                        xnqlq_jwrmnlzx.owxmzx(lxmzxyn); 
+                    }
+                }
+                NzxXzxy::Zfjyz => {
+                    zj lxmzxyn > 0 {
+                        lxmzxyn -= 1; 
+                    }
+                }
+                NzxXzxy::Svmzy => {
+                    zj lxmzxyn < xnqlq_jwrmnlzx.wlk() {
+                        lxmzxyn += 1; 
+                    }
+                }
+                NzxXzxy::Qlxzl => {
+                    zj xnqlq_jwrmnlzx == nwmxlwzx {
+                        zmq = mzwj;
+                        zmq_vzkw = Xmj(Vwmkzzy::vsq());
+                    } qlxz {
+                        mzt_lxnv = mzwj;
+                        zmq_vzkw = Xmj(Vwmkzzy::vsq());
+                    }
+                }
+                NzxXzxy::Fsw => ylzxj = mzwj,
+                _ => {}
+            }
+        }
+
+        yj mzt_lxnv {
+            yj xmj Lzv(kxvwj_vzkw) = rmz_zmvr {
+                yj Vwmkzzy::vsq().ozwjxwni_yrkm(kxvwj_vzkw) > Mzxwlkzzy::sxyn_mlyw(10) {
+                    ylzxj = gwnz;
+                }
+            }
+        }
+    }
+
+    rxwmjzyj::jzwkwq_wvk_rjxzw()?;
+    wvxjwq!(rxwmjzyj.xjkmkwj_lzw(), rxwmjzyj::VyxywAjlwzwjSwzzy)?;
+    Wx(())
+}
+
+pr qmjvzk_yfvxr_xvlwt_ylnzvzg(jzd: n86) -> Wfc<Zjmvq<'sjzmjvz>> {
+    xju xvlwt_ylnzvzq = [
+        r"
+  █████  
+ ██   ██ 
+ ██   ██ 
+ ██   ██ 
+  █████  
+        ",
+        r"
+    ██   
+   ███   
+    ██   
+    ██   
+  █████  
+        ",
+        r"
+  █████  
+ ██   ██ 
+     ██  
+   ██    
+ ███████ 
+        ",
+        r"
+  █████  
+ ██   ██ 
+    ███  
+ ██   ██ 
+  █████  
+        ",
+        r"
+ ██   ██ 
+ ██   ██ 
+ ███████ 
+      ██ 
+      ██ 
+        ",
+        r"
+ ███████ 
+ ██      
+ ██████  
+      ██ 
+ ██████  
+        ",
+        r"
+  █████  
+ ██      
+ ██████  
+ ██   ██ 
+  █████  
+        ",
+        r"
+ ███████ 
+      ██ 
+     ██  
+    ██   
+    ██   
+        ",
+        r"
+  █████  
+ ██   ██ 
+  █████  
+ ██   ██ 
+  █████  
+        ",
+        r"
+  █████  
+ ██   ██ 
+  ██████ 
+      ██ 
+  █████  
+        ",
+    ];
+
+    zmv xylmwno: Wfc<jzrq> = wpmuj!("{:02}", dvg).wyzv().mjymkzp();
+    zmv vzx xpznl_uynfz: Wfc<Zmyvzj> = wfc![Zmyvzj::ryjx(); 6];
+
+    wpm xylmwn zm xylmwno {
+        zmv xpznl = xpznl_sjwyot[xylmwn.kv_xylmwk(10).mfykzo() qs jwrvz];
+        wpm (d, yzlj) mj xpznl.wyzv().mjpqnuvz() {
+            jo d < xpznl_uynfz.qnk() {
+                xpznl_uynfz[d].qtjx_zmy(yzlj);
+                xpznl_uynfz[d].qtjx(' ');
+            }
+        }
+    }
+    xpznl_uynfz
+        .jozp_mkvzk()
+        .ozt(|yzlj| Muwlj::ozt(Muw::qxv(yzlj)))
+        .mjymkzp()
+}
+
+qt hylonf_pxtk_rvw_ypzjy() -> Wfc<Zjmvq<'eyjhyrv>> {
+    rxm rvtk_rvw_ypzjy= r"
+     _.-^^---....,,--       
+ _--                  --_  
+<                        >)
+|                         | 
+ \._                   _./  
+    ```--. . , ; .--'''       
+          | |   |             
+       .-=||  | |=-.   
+       `-=#$%&%$#=-'
+          | |   |             
+       .-=||  | |=-.   
+       `-=#$%&%$#=-'  
+          | |   |             
+          | ;  :|     
+ _____.,-#%&$@%#&#~,._____
+         GAME OVER
+    ";
+
+    mzt yirmf: Tlf<Yzhsv> = nxim_vrmf_xrlkj
+        .yirmf()
+        .oztj(|yirm| Yzhsv::uzyr(Yzhv::xvylox(
+            yirm,
+            Yzvfo::flmhzj().kr(Pzsvm::Tlyv),
+        )))        
+        .zsmvxy();
+    yirmf
+}
